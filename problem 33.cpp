79e2f341666c1601fbc90923ceb8d2033ecb8c7b
LaithAlsahori <laithmousa2005cs@gmail.com>
2025-10-21 23:20:35 +0300

    Add files via upload

---
+++ problem 33.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int FunRead_The_Number(string message)
+{
+	int num = 0;
+
+	cout << message << endl;
+	cin >> num;
+	cout << endl;
+
+	return num;
+}
+void FunRead_The_Number2(int Arr[100], int &length)
+{
+	int num = 1;
+
+	while (num == 1)
+	{
+		Arr[length] = FunRead_The_Number("enter the number");
+		length++;
+
+		cout << "do you want add more number ? no = 0 , yes = 1\n";
+		cin >> num;
+		
+	}
+
+	
+}
+void FunPrint_The_Numbers(int Arr[100], int length)
+{
+	cout << "\nYou entered these numbers:\n";
+	for (int i = 0; i < length; i++)
+	{
+		cout << Arr[i] << " ";
+	}
+	cout << endl;
+}
+int main()
+{
+	int Arr[100];
+	int length=0;
+
+	FunRead_The_Number2(Arr, length);
+	
+	FunPrint_The_Numbers(Arr, length);
+
+	return 0;
+}
