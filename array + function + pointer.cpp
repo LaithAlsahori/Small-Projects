c2c98903a420298fe1a82604a67c6f6e6d440c2e
LaithAlsahori <laithmousa2005cs@gmail.com>
2026-02-25 21:54:32 +0300

    Add files via upload
    This is a simple project I developed while learning the fundamentals of programming using C++. The project demonstrates the use of pointers, functions, and arrays, and helps reinforce core concepts such as memory access, data handling, and function-based program structure.

---
+++ array + function + pointer.cpp
@@ -0,0 +1,49 @@
+﻿#include<iostream>
+using namespace std;
+int FunRead_The_Positive_Number()
+{
+	int num = 0;
+
+		cout << "Enter positive number : ";
+		cin >> num;
+
+	return num;
+}
+void FunFill_Numbers_In_Array(int arr[],int &length)
+{
+	cout << "enter how many numbers you want enter : ";
+	cin >> length;
+
+	for (int i = 0; i < length; i++)
+	{
+		arr[i]=FunRead_The_Positive_Number();
+	}
+
+}
+void FunPrint_The_Number(int arr[],int& length,int *ptr)
+{
+
+	cout << "\n\nthe numbers in array : ";
+
+	for (int i = 0; i < length; i++)
+	{
+		cout << *(ptr + i) << " ";
+	}
+}
+void start(int arr[],int &length,int *ptr)
+{
+	FunFill_Numbers_In_Array(arr,length);
+	FunPrint_The_Number(arr, length, ptr);
+}
+int main()
+{
+	int arr[100]; //vectorمن الافضل هنا استخدام ال 
+	int length = 0;
+	int* ptr;
+
+	ptr = arr;
+
+	start(arr, length, ptr);
+
+	return 0;
+}
